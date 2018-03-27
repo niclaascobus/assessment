9a63e2e9b6f7aaac147703a838548e6592a28bc0
nico <nico@localhost.localdomain>
2018-03-27 18:16:22 +0200

    Created first unit test

---
+++ src/test/java/input/InputFileReaderTest.java
@@ -0,0 +1,16 @@
+package input;
+
+import org.junit.Test;
+
+public class InputFileReaderTest {
+
+    @Test
+    public void testInputFileReaderNumberOfLines()   {
+        //create some files and then read them checking the correct number of lines - delete them afterwards
+    }
+
+    @Test
+    public void testInputFileReaderNoFile()   {
+        //pass bogus file name and check for exception
+    }
+}
